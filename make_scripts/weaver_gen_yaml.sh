372a69723c88ca98d987df21863b49d024de94ab
Ethan Berkley <ethanberkley@gmail.com>
2024-11-06 17:52:16 +0000

    Added ability to enable / disable vtune profiling in .env.

--- make_scripts/weaver_gen_yaml.sh
+++ make_scripts/weaver_gen_yaml.sh
@@ -2,6 +2,13 @@
 sed -i "s#<DOCKER>#$DOCKER#g" $KUBE_GEN_YAML # Just in case...
 sed -i "s#<OB_CORES>#$OB_CORES#g" $KUBE_GEN_YAML
 sed -i "s#<OB_REPLICAS>#$OB_REPLICAS#g" $KUBE_GEN_YAML
+if [[ ! $KUBE = "weaver-kube" ]]; then
+	echo "using modified weaver kube deployer..."
+	sed -i "s#<VTUNE_SNIPPET>#VTuneEnabled: $VTUNE_ENABLED#g" $KUBE_GEN_YAML
+else
+	# Using default weaver kube installation, so no vtune support.
+	sed -i "s#<VTUNE_SNIPPET>##g" $KUBE_GEN_YAML
+fi
 yaml=$($KUBE deploy $KUBE_GEN_YAML 2>>$LOGS_FILE) 
 deployment=$(echo $yaml | sed 's/\/tmp\/kube_\([0-9a-z]\+\)\.yaml/\1/g')
 echo version = $deployment | tee -a $LOGS_FILE
