3cfd7757de0cdbebcc0e8a34ecdc62b8115a53e6
Ethan Berkley <ethantberkley@gmail.com>
2025-01-13 12:23:33 -0500

    Added support for setting values in generated yamls

--- make_scripts/load_gen_yaml.sh
+++ make_scripts/load_gen_yaml.sh
@@ -33,6 +33,9 @@ sed -i "s/<LOADGEN_REPLICAS_ENV>/\"$LOADGEN_REPLICAS\"/g" $LOAD_GEN_YAML
 # Tell the locust instances which of the loadshapes to run.
 sed -i "s/<LOCUST_SHAPE>/$LOCUST_SHAPE/g" $LOAD_GEN_YAML
 
+# Set the wait time for the rampload shape
+sed -i "s#<LOCUST_WAIT_TIME>#\"$LOCUST_WAIT_TIME\"#g" $LOAD_GEN_YAML
+
 # Build the image and push it to docker
 docker build $LOAD_SRC -t $DOCKER/loadgen:$version >>$DEBUG_OUTPUT 2>&1
 docker push $DOCKER/loadgen:$version >>$DEBUG_OUTPUT
