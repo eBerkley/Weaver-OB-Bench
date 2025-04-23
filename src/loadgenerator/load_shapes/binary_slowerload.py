7c6b891aa09083a8aa94610d94ead5d42bc2d80a
Ethan Berkley <ethantberkley@gmail.com>
2025-04-23 13:08:47 -0400

    updated binary_slowerload

--- src/loadgenerator/load_shapes/binary_slowerload.py
+++ src/loadgenerator/load_shapes/binary_slowerload.py
@@ -51,6 +51,10 @@ class BinarySlowLoad(LoadTestShape):
 
         self._invalid: float = 0.0
         """Current candidate for min unsustainable throughput"""
+
+        self._fallback_target: float = 0.0
+        """What value do we scale down to?"""
+
         super().__init__(*args, **kwargs)
 
     def tick(self) -> Optional[Tuple[int, float]]:
@@ -68,31 +72,32 @@ class BinarySlowLoad(LoadTestShape):
         if self._p99 == None:
             self._p99 = 0
 
-        if not self._searching:
+        if not self._searching: # We haven't seen an unsustainable throughput yet
             log_string += f"users: {cur_users} \t P50: {self._p50} \t P99: {self._p99} \t SLO: {self._slo_timer} "
 
-            if self._slo_timer <= 0:
+            if self._slo_timer <= 0: # We just found one
                 self._searching = True
                 log_string += f"BEGINNING SEARCH!"
                 self._invalid = cur_users
                 
                 self._slo_timer = WAIT_TIME
                 self._user_secs = 0
-                self._target = self._valid
+                self._fallback_target = self._valid - self.ramp_amount
+                self._target = self._fallback_target
 
-            elif self._p99 > self.max_tail:
+            elif self._p99 > self.max_tail: # We are currently violating SLO
                 log_string += f"SLO Vioilating..."
                 self._slo_timer -= 1
                 self._target = cur_users
             
-            elif self._user_secs >= self.pause_time:
+            elif self._user_secs >= self.pause_time: # We are within SLO, and waiting
                 self._slo_timer = WAIT_TIME
                 self._target = cur_users + self.ramp_amount
                 self._user_secs = 0
                 self._valid = cur_users
                 log_string += f"stabilized, ramping up."
                 
-            else:
+            else: # We are within SLO, and we just finished waiting.
                 self._user_secs += 1
                 self._slo_timer = WAIT_TIME
                 log_string += f"t: {self._user_secs}"
@@ -101,25 +106,27 @@ class BinarySlowLoad(LoadTestShape):
         else: # self._searching == True
             log_string += f"users: {cur_users} \t valid: {self._valid} \t P50: {self._p50} \t P99: {self._p99} \t SLO: {self._slo_timer} "
             if self._slo_timer <= 0:
-                if self.search_granularity > cur_users - self._valid:
+                if self.search_granularity > cur_users - self._valid: # We found a termination point!
                     return None
                 log_string += f"\t Invalid. Ramping down..."
                 self._invalid = cur_users
 
                 self._slo_timer = WAIT_TIME / 2
                 self._user_secs = 0
-                self._target = self._valid
+                self._target = self._fallback_target
             
-            elif self._p99 > self.max_tail:
+            elif self._p99 > self.max_tail: 
                 log_string += f"SLO Vioilating..."
                 self._slo_timer -= 1
                 self._target = cur_users
             
             elif self._user_secs >= self.pause_time:
                 self._slo_timer = WAIT_TIME / 2
-                self._target = (cur_users + self._invalid) // 2
+                self._valid = max(self._valid, cur_users)
+
+                self._target = (self._valid + self._invalid) // 2
+
                 self._user_secs = 0
-                self._valid = cur_users
                 log_string += f"stabilized, ramping up."
                 
             else:
@@ -127,7 +134,7 @@ class BinarySlowLoad(LoadTestShape):
                 # self._slo_timer = WAIT_TIME / 2 # We don't want to make it too easy to complete a ramp up
                 log_string += f"t: {self._user_secs}"
                 self._target = cur_users
-                 
+                
             
         logging.info(log_string)
         return self._target, self.ramp_rate
