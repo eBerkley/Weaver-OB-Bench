e9331ba42e936f34c947727311189555a2ff43c9
Ethan <ethantberkley@gmail.com>
2025-07-07 11:15:56 -0400

    walk.py now supports SLA mode w/ -a flag.

--- benchmark/walk.py
+++ benchmark/walk.py
@@ -39,27 +39,47 @@ class Group:
             if a not in self.comps:
                 return a
 
-mode, scheme, _, _, user_count, value, breadth, prune = util.get_args()
+mode, scheme, throughput_mode, _, user_count, value, breadth, prune = util.get_args()
 scheme = Group(scheme)
 
-user_count  = user_count or 25_000
-value       = value      or 'p99'
+if throughput_mode: 
+    SLA = user_count or 100
+else: 
+    user_count = user_count or 25_000
+
+value = value or 'p99'
+
+extract_metric: Callable[[util.Val], float] = None
+match value:
+    case "p50":
+        extract_metric = lambda x: x.p50
+    case "p99":
+        extract_metric = lambda x: x.p99
+    case "cpu":
+        extract_metric = lambda x: x.cpu
 
 _idx = -1
 def get_idx(dl: util.DataList):
-    global _idx
-    if _idx != -1:
-        return _idx
+    if throughput_mode:
+        for i in range(len(dl)):
+            if extract_metric(dl[i]) >= SLA:
+                return max(i - 1, 0)
+        return len(dl)
 
-    if dl.ds[-1].get_users() < user_count:
-        return 99
+    else:
+        global _idx
+        if _idx != -1:
+            return _idx
 
-    for j in range(len(dl)):
-        if dl.ds[j].get_users() == user_count:
-            _idx = j
-            return j
-    
-    return len(dl)
+        if dl.ds[-1].get_users() < user_count:
+            return 99
+
+        for j in range(len(dl)):
+            if dl.ds[j].get_users() == user_count:
+                _idx = j
+                return j
+        
+        return len(dl)
 
 # makes it so that if a benchmark didn't actually reach the user
 # count we are trying to optimize, the fake value we give
@@ -72,14 +92,10 @@ def weight_mst(f: float, l: int):
         return f - l
     return f
 
-srt: Callable[[util.DataList], float] = None
-match value:
-    case "p50":
-        srt = lambda x: weight_mst(x[get_idx(x)].p50, len(x))
-    case "p99":
-        srt = lambda x: weight_mst(x[get_idx(x)].p99, len(x))
-    case "cpu":
-        srt = lambda x: weight_mst(x[get_idx(x)].cpu, len(x))
+def srt(dl: util.DataList) -> float:
+    if throughput_mode:
+        return dl.ds[int(get_idx(dl))].get_users()
+    return weight_mst(extract_metric(dl[get_idx(dl)]), len(dl))
 
 _next_cache: Dict[str, List[Group]] = {}
 def get_next(s: Group) -> List[Group]:
@@ -97,6 +113,8 @@ def get_next(s: Group) -> List[Group]:
     _next_cache[str(s)] = nxt
     return nxt
 
+# In latency minimizing mode, gets the largest throughput that has latency / util < SLA.
+# Else, gets the latency / util at the throughput specified.
 _cache: Dict[str, util.DataList] = {}
 _benchmarks = 0
 def get_val(s: Group) -> float:
@@ -111,7 +129,7 @@ def get_val(s: Group) -> float:
     _cache[str(s)] = dl
     return srt(dl)
 
-def walk(init: Group, bad_list: List[str]) -> Group:
+def walk_latency(init: Group, bad_list: List[str]) -> Group:
 
     children = get_next(init)
     next_batch: List[int] = []
@@ -120,7 +138,7 @@ def walk(init: Group, bad_list: List[str]) -> Group:
 
         if new_c in bad_list:
             continue
-        
+
         if get_val(children[i]) > get_val(init):
             bad_list.append(new_c)
         else:
@@ -134,29 +152,88 @@ def walk(init: Group, bad_list: List[str]) -> Group:
     for i in range(min(len(next_batch), breadth)):
         n = next_batch[i]
         if prune:
-            b = walk(children[n], bad_list.copy())
+            b = walk_latency(children[n], bad_list.copy())
         else:
-            b = walk(children[n], [])
+            b = walk_latency(children[n], [])
         if get_val(b) < get_val(best):
             best = b
     return best
 
-if __name__ == '__main__':
-    winner = walk(scheme, [])
-    v = get_val(winner)
-    if mode == util.Mode.TERM.value:
-        if v > 9_000:
-            print(f'"Winner": {str(winner)}, MST = {_cache[str(winner)].ds[-2].users}"')
+def walk_throughput(init: Group, bad_list: List[str]) -> Group:
+
+    children = get_next(init)
+    next_batch: List[int] = []
+    for i in range(len(children)):
+        new_c = init.difference(children[i])
+
+        if new_c in bad_list:
+            continue
+        
+        if get_val(children[i]) < get_val(init):
+            bad_list.append(new_c)
+        elif get_val(children[i]) == get_val(init):
+            
+            chdl=_cache[str(children[i])]
+            initdl=_cache[str(init)]
+            chmet = extract_metric(chdl[get_idx(chdl)])
+            initmet = extract_metric(initdl[get_idx(initdl)])
+            if chmet > initmet:
+                bad_list.append(new_c)
         else:
-            print(f'Winner: {str(winner)} = {v}')
+            next_batch.append(i)
     
-    elif mode == util.Mode.CSV.value: 
-        print(f"{user_count},{prune},{breadth},{value},{_benchmarks},{get_val(winner)}")
-    elif mode == util.Mode.AVG.value: 
-        # not really doing any average, but we use this when
-        # we want to compare walk results with an average.
-        print(v, end="")
-    
+    if len(next_batch) == 0:
+        return init
+
+    next_batch.sort(key=lambda i: get_val(children[i]))    
+    best = children[next_batch[0]]
+    for i in range(min(len(next_batch), breadth)):
+        n = next_batch[i]
+        if prune:
+            b = walk_throughput(children[n], bad_list.copy())
+        else:
+            b = walk_throughput(children[n], [])
+        if get_val(b) > get_val(best):
+            best = b
+        elif get_val(b) == get_val(best):
+            
+            bdl=_cache[str(b)]
+            bestdl=_cache[str(best)]
+            bmet = extract_metric(bdl[get_idx(bdl)])
+            bestmet = extract_metric(bestdl[get_idx(bestdl)])
+
+            # print(f"(@{get_val(best)}) b: {b} = {bmet}, best: {best} = {bestmet}")
+            if bmet < bestmet:
+                best = b
+    return best
+
+
+if __name__ == '__main__':
+    if throughput_mode:
+        winner = walk_throughput(scheme, [])
+        v = get_val(winner)
+        dl=_cache[str(winner)]
+        metric = extract_metric(dl[get_idx(dl)])
+        print(f'Winner: {str(winner)} = {v}, {value} = {metric}')
+
+    else:
+        winner = walk_latency(scheme, [])
+        v = get_val(winner)
+        if mode == util.Mode.TERM.value:
+            if v > 9_000:
+                print(f'"Winner": {str(winner)}, MST = {_cache[str(winner)].ds[-2].users}"')
+            
+                
+            else:
+                print(f'Winner: {str(winner)} = {v}')
+        
+        elif mode == util.Mode.CSV.value: 
+            print(f"{user_count},{prune},{breadth},{value},{_benchmarks},{get_val(winner)}")
+        elif mode == util.Mode.AVG.value: 
+            # not really doing any average, but we use this when
+            # we want to compare walk results with an average.
+            print(v, end="")
+        
     import sys
     print("total benchmarks =", _benchmarks, file=sys.stderr)
     
